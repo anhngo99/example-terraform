9ecd3f84b5b6ac6a264be8be30e866fda9b57e41
anhngo99 <110527325+anhngo99@users.noreply.github.com>
2022-08-10 16:20:48 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "aws_ami" "ubuntu" {
 
 resource "null_resource" "install-azureCli" {
  provisioner "local-exec" {
-       command = "curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash"
+       command = "pwd && ls"
 #    <<EOH
 #                   curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 #                    az --version
