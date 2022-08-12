725b8739f794042b127494cbe80ec746df3a3a8b
anhngo99 <110527325+anhngo99@users.noreply.github.com>
2022-08-12 09:43:37 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,10 +18,10 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "null_resource" "install-fdbgdfgdf" {
+resource "null_resource" "install-dvdsfsdfsdfs" {
  provisioner "local-exec" {
        command = <<EOH
-                    cd /tmp && wget https://www.python.org/ftp/python/3.7.5/Python-3.7.5.tgz && tar -xf Python-3.8.3.tgz && cd python-3.8.3 && ./configure --enable-optimizations && sudo make altinstall && python3 --version
+                    cd /tmp && wget https://www.python.org/ftp/python/3.8.3/Python-3.8.3.tgz && tar -xf Python-3.8.3.tgz && cd python-3.8.3 && ./configure --enable-optimizations && sudo make altinstall && python3 --version
                     
                   EOH
 #   az --version
