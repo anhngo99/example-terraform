01f896bf123bbd672e1221083bea7c6be1c37b41
anhngo99 <110527325+anhngo99@users.noreply.github.com>
2022-08-12 10:56:32 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,11 +18,11 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "null_resource" "install-dscsdcsdcsd" {
+resource "null_resource" "install-xcxcxcx" {
  provisioner "local-exec" {
        command = <<EOH
                     cd /tmp && wget https://www.python.org/ftp/python/3.8.3/Python-3.8.3.tgz && tar -xf Python-3.8.3.tgz && 
-                    cd /tmp/Python-3.8.3 &&ls -la && make altinstall && python3 --version
+                    cd /tmp/Python-3.8.3 &&ls -la && ./install-sh && python3 --version
                   EOH
 #   az --version
 #   curl -sL https://aka.ms/InstallAzureCLIDeb | bash
